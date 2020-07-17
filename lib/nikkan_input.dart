98611e3c1478c8832ee87c4b062a2c0e068bd7e8
oka331 <oka331@gmail.com>
2020-07-17 23:43:34 +0900

    第６版：生年月日から日干を算出する

--- lib/nikkan_input.dart
+++ lib/nikkan_input.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 
 class NikkanInput extends StatelessWidget {
   final myFocusNode = FocusNode();
-  String name;
   final myController = TextEditingController();
+
+  String name;
+  String SeiNen = '1957';
+  String SeiTuki = '3';
+  String SeiHi = '31';
+  String SeiNenGatuHi = '1957年3月31日';
+  String Nikkan = '0';
+  DateTime Date2 = DateTime.now();
+  DateTime Date0 = DateTime(1900, 1, 1);
+  DateTime Date1 = DateTime(1900, 1, 1);
+  int seinen = 1957;
+  int seituki = 3;
+  int seihi = 31;
+  int nissuu = 0;
+  int nikkan = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +28,7 @@ class NikkanInput extends StatelessWidget {
       ),
       body: Container(
         child: Padding(
-          padding: const EdgeInsets.all(32.0),
+          padding: const EdgeInsets.all(12.0),
           child: Column(
             children: <Widget>[
               TextField(
@@ -23,29 +37,53 @@ class NikkanInput extends StatelessWidget {
                       //border: InputBorder.none,
                       hintText: '年'),
                   onChanged: (text) {
-                    name = text;
-                    print("$name");
+                    SeiNen = text;
+                    print("$SeiNen");
                   }),
               TextField(
+                  decoration: InputDecoration(
+                      //border: InputBorder.none,
+                      hintText: '月'),
+                  onChanged: (text) {
+                    SeiTuki = text;
+                    print("$SeiTuki");
+                  }),
+              TextField(
+                  decoration: InputDecoration(
+                      //border: InputBorder.none,
+                      hintText: '日'),
+                  onChanged: (text) {
+                    SeiHi = text;
+                    print("$SeiHi");
+                  }),
+              /*TextField(
+                decoration: InputDecoration(
+                    //border: InputBorder.none,
+                    hintText: '日'),
                 controller: myController,
-                focusNode: myFocusNode,
-              ),
+                //focusNode: myFocusNode,
+              ),*/
               RaisedButton(
-                child: Text('新規登録'),
+                child: Text('入力完了'),
                 onPressed: () {
                   //
+                  SeiNenGatuHi = SeiNen + '年' + SeiTuki + '月' + SeiHi + '日';
                   //myFocusNode.requestFocus();
-                  //print(myController.text);
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        // Retrieve the text the user has entered by using the
-                        // TextEditingController.
-                        content: Text(myController.text),
-                      );
-                    },
-                  );
+                  print('$SeiNenGatuHi');
+
+                  print('$Date2');
+                  print('$Date0');
+                  seinen = int.parse(SeiNen);
+                  seituki = int.parse(SeiTuki);
+                  seihi = int.parse(SeiHi);
+                  Date1 = DateTime(seinen, seituki, seihi);
+
+                  nissuu = Date1.difference(Date0).inDays;
+                  nikkan = nissuu % 10;
+
+                  print('$nissuu');
+                  print('$nikkan');
+                  //print('$Date2');
                 },
               ),
             ],
