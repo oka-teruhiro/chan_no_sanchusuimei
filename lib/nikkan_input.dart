c9491e1e2889a19c7fae135c00ac20bc0e1788d5
oka331 <oka331@gmail.com>
2020-07-18 08:25:16 +0900

    第７版：生年月日から日干を算出して画面遷移する

--- lib/nikkan_input.dart
+++ lib/nikkan_input.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp5/nikkan_hinoe.dart';
+import 'package:flutterapp5/nikkan_hinoto.dart';
+import 'package:flutterapp5/nikkan_kanoe.dart';
+import 'package:flutterapp5/nikkan_kanoto.dart';
+import 'package:flutterapp5/nikkan_kinoe.dart';
+import 'package:flutterapp5/nikkan_kinoto.dart';
+import 'package:flutterapp5/nikkan_mizunoe.dart';
+import 'package:flutterapp5/nikkan_mizunoto.dart';
+import 'package:flutterapp5/nikkan_tsutinoto.dart';
+
+import 'nikka_tsutinoe.dart';
 
 class NikkanInput extends StatelessWidget {
   final myFocusNode = FocusNode();
@@ -66,23 +77,96 @@ class NikkanInput extends StatelessWidget {
               RaisedButton(
                 child: Text('入力完了'),
                 onPressed: () {
-                  //
-                  SeiNenGatuHi = SeiNen + '年' + SeiTuki + '月' + SeiHi + '日';
-                  //myFocusNode.requestFocus();
-                  print('$SeiNenGatuHi');
-
-                  print('$Date2');
-                  print('$Date0');
                   seinen = int.parse(SeiNen);
                   seituki = int.parse(SeiTuki);
                   seihi = int.parse(SeiHi);
                   Date1 = DateTime(seinen, seituki, seihi);
-
                   nissuu = Date1.difference(Date0).inDays;
                   nikkan = nissuu % 10;
 
+                  print('$Date0');
+                  print('$Date1');
                   print('$nissuu');
                   print('$nikkan');
+                  print(Nikkan);
+
+                  Nikkan = 'NikkanTsutinoe';
+                  print(Nikkan);
+
+                  if (nikkan == 0) {
+                    print('甲');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanKinoe()),
+                    );
+                  } else if (nikkan == 1) {
+                    print('乙');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanKinoto()),
+                    );
+                  } else if (nikkan == 2) {
+                    print('丙');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanHinoe()),
+                    );
+                  } else if (nikkan == 3) {
+                    print('丁');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanHinoto()),
+                    );
+                  } else if (nikkan == 4) {
+                    print('戊');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanTsutinoe()),
+                    );
+                  } else if (nikkan == 5) {
+                    print('己');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => NikkanTsutinoto()),
+                    );
+                  } else if (nikkan == 6) {
+                    print('庚');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanKanoe()),
+                    );
+                  } else if (nikkan == 7) {
+                    print('辛');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanKanoto()),
+                    );
+                  } else if (nikkan == 8) {
+                    print('壬');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanMizunoe()),
+                    );
+                  } else {
+                    print('癸');
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NikkanMizunoto()),
+                    );
+                  }
+
+                  //gamenSenniNikkan(nikkan);
                   //print('$Date2');
                 },
               ),
@@ -92,4 +176,13 @@ class NikkanInput extends StatelessWidget {
       ),
     );
   }
+
+  /*void gamenSenniNikkan(int nikkan) {
+    print('北');
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NikkanTsutinoe()),
+    );
+  }:*/
 }
