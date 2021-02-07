eddfe006553a1d00cb72f248707d0650baae3c30
oka331 <oka331@gmail.com>
2021-02-07 14:00:18 +0900

    第27版　クローズテスト２（１．２．０）2021.2.7

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('チャンの三柱推命 v2.7'),
+        title: Text('チャンの三柱推命'),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.navigate_next),
--- lib/nikkan_hinoe.dart
+++ lib/nikkan_hinoe.dart
@@ -6,7 +6,7 @@ class NikkanHinoe extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('日干：丙（ひのえ'),
+        title: Text('日干：丙（ひのえ)'),
       ),
       body: Container(
         height: double.infinity,
@@ -14,64 +14,42 @@ class NikkanHinoe extends StatelessWidget {
         child: ListView(
           children: <Widget>[
             ListTile(
-              title: Text(
-                  '　日干が丙の人は、太陽にたとえられる性質を持っています。'
-              ),
+              title: Text('　日干が丙の人は、太陽にたとえられる性質を持っています。'),
             ),
             ListTile(
-              title: Image.asset(
-                  'images/丙_太陽.jpg'
-              ),
+              title: Image.asset('images/丙_太陽.jpg'),
             ),
             ListTile(
-              title: Text(
-                  '　太陽は明るく輝き世界を照らしています。ですから、心が広くて小さなことに'
-                      'はこだわらず、元気で明るくハツラツとしています。'
-              ),
+              title: Text('　太陽は明るく輝き世界を照らしています。ですから、心が広くて小さなことに'
+                  'はこだわらず、元気で明るくハツラツとしています。'),
             ),
             ListTile(
-              title: Text(
-                  '　情熱的で感情が豊かです。太陽が沈んでも、また翌朝、日が昇るように、気性'
-                      'はさっぱりとしていてしつこくなく、執着心は弱いほうです。嫌なことが'
-                      'あったとしても基本的に一晩寝たら忘れてしまうでしょう。お金にも執着'
-                      'はなく、気前がよくて何かと浪費しがちです。'
-              ),
-            ),
-
-            ListTile(
-              title: Image.asset(
-                  'images/丙_朝日.jpg'
-              ),
+              title: Text('　情熱的で感情が豊かです。太陽が沈んでも、また翌朝、日が昇るように、気性'
+                  'はさっぱりとしていてしつこくなく、執着心は弱いほうです。嫌なことが'
+                  'あったとしても基本的に一晩寝たら忘れてしまうでしょう。お金にも執着'
+                  'はなく、気前がよくて何かと浪費しがちです。'),
             ),
             ListTile(
-              title: Text(
-                  '　日が射すと、すべての物が白日の下にさらされるように、開けっぴろげで隠'
-                      'し事はできないたちで、他人から見ると、とてもわかりやすい人です。内'
-                      '緒にしてもすぐにばれてしまうでしょう。'
-              ),
+              title: Image.asset('images/丙_朝日.jpg'),
             ),
             ListTile(
-              title: Text(
-                  '　親切で寛大な性格で、人から好感を持たれます。活動的で積極的です。太陽'
-                      'がどこからでも見えるように、どこにいても目立ちます。行動は速いです'
-                      'が、短気で早合点するような、せっかちな面もあります。何事も結論を急'
-                      'ぎ、はっきりとさせたがります。'
-              ),
+              title: Text('　日が射すと、すべての物が白日の下にさらされるように、開けっぴろげで隠'
+                  'し事はできないたちで、他人から見ると、とてもわかりやすい人です。内'
+                  '緒にしてもすぐにばれてしまうでしょう。'),
             ),
             ListTile(
-              title: Text(
-                  '　そのときの感情によって一喜一憂するような、気分屋なところもあります。'
-                      'いばったり、情深くなったり、気分にムラが目立ちます。'
-              ),
+              title: Text('　親切で寛大な性格で、人から好感を持たれます。活動的で積極的です。太陽'
+                  'がどこからでも見えるように、どこにいても目立ちます。行動は速いです'
+                  'が、短気で早合点するような、せっかちな面もあります。何事も結論を急'
+                  'ぎ、はっきりとさせたがります。'),
             ),
-
-
             ListTile(
-              title: Text(
-                  ''
-              ),
+              title: Text('　そのときの感情によって一喜一憂するような、気分屋なところもあります。'
+                  'いばったり、情深くなったり、気分にムラが目立ちます。'),
             ),
-
+            ListTile(
+              title: Text(''),
+            ),
             RaisedButton(
               child: Text('戻る'),
               onPressed: () {
@@ -80,10 +58,7 @@ class NikkanHinoe extends StatelessWidget {
             ),
           ],
         ),
-
       ),
-
     );
   }
-
 }
