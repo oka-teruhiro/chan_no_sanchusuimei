f97f13d49a2a7fa27d9c106cc8c0a3a7fec968e7
oka331 <oka331@gmail.com>
2021-02-07 10:12:30 +0900

    第26版　乙・丙のアップバー修正・メイン画面バージョン削除

--- lib/nikkan_kinoto.dart
+++ lib/nikkan_kinoto.dart
@@ -6,7 +6,7 @@ class NikkanKinoto extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('日干：乙（きのと'),
+        title: Text('日干：乙（きのと)'),
       ),
       body: Container(
         height: double.infinity,
@@ -14,64 +14,42 @@ class NikkanKinoto extends StatelessWidget {
         child: ListView(
           children: <Widget>[
             ListTile(
-              title: Text(
-                  '　日干が乙の人は、草花やつる草にたとえられる性格を持っています。'
-              ),
+              title: Text('　日干が乙の人は、草花やつる草にたとえられる性格を持っています。'),
             ),
             ListTile(
-              title: Image.asset(
-                  'images/乙_イラスト１.jpg'
-              ),
+              title: Image.asset('images/乙_イラスト１.jpg'),
             ),
             ListTile(
-              title: Text(
-                  '　つる草は柔らかく、弱々しい印象です。性格はつる草がしなやかなように、柔'
-                      '和で、草花のように表現力があります。現実をわきまえ、自分が置かれた'
-                      '環境の中で発展していこうと考えます。'
-              ),
+              title: Text('　つる草は柔らかく、弱々しい印象です。性格はつる草がしなやかなように、柔'
+                  '和で、草花のように表現力があります。現実をわきまえ、自分が置かれた'
+                  '環境の中で発展していこうと考えます。'),
             ),
             ListTile(
-              title: Text(
-                  '　つる草が他の木々や壁に従って伸びていくように、反応が速くて適応力があ'
-                      'り、どのような環境でも対応することができます。その反面、つる草自身'
-                      'で自立しないように、周囲の意見に妥協しやすく、行動が他人によって拘'
-                      '束されがちです。'
-              ),
-            ),
-
-            ListTile(
-              title: Image.asset(
-                  'images/乙_からみつく.jpg'
-              ),
+              title: Text('　つる草が他の木々や壁に従って伸びていくように、反応が速くて適応力があ'
+                  'り、どのような環境でも対応することができます。その反面、つる草自身'
+                  'で自立しないように、周囲の意見に妥協しやすく、行動が他人によって拘'
+                  '束されがちです。'),
             ),
             ListTile(
-              title: Text(
-                  '　つる草が絡みついていくように、表面はおとなしく見えても、内面は独占欲'
-                      'が強く、嫉妬心や執念深いところがあります。周りの人の出方によって態'
-                      '度を変える、といった面も見られます。'
-              ),
+              title: Image.asset('images/乙_からみつく.jpg'),
             ),
             ListTile(
-              title: Text(
-                  '　案外、お金に対して敏感なところがあります。金銭はコツコツ努力して蓄える'
-                      'ほうで、浪費はあまりしません。'
-              ),
+              title: Text('　つる草が絡みついていくように、表面はおとなしく見えても、内面は独占欲'
+                  'が強く、嫉妬心や執念深いところがあります。周りの人の出方によって態'
+                  '度を変える、といった面も見られます。'),
             ),
             ListTile(
-              title: Text(
-                  '　性格はやさしくて慈愛に満ちあふれていますが、気弱になって依頼心を起こ'
-                      'すことも多いでしょう。とかく自信を失いやすい性格です。誘惑に弱く、だ'
-                      'まされやすい面もあります。'
-              ),
+              title: Text('　案外、お金に対して敏感なところがあります。金銭はコツコツ努力して蓄える'
+                  'ほうで、浪費はあまりしません。'),
             ),
-
-
             ListTile(
-              title: Text(
-                  ''
-              ),
+              title: Text('　性格はやさしくて慈愛に満ちあふれていますが、気弱になって依頼心を起こ'
+                  'すことも多いでしょう。とかく自信を失いやすい性格です。誘惑に弱く、だ'
+                  'まされやすい面もあります。'),
             ),
-
+            ListTile(
+              title: Text(''),
+            ),
             RaisedButton(
               child: Text('戻る'),
               onPressed: () {
@@ -80,10 +58,7 @@ class NikkanKinoto extends StatelessWidget {
             ),
           ],
         ),
-
       ),
-
     );
   }
-
 }
